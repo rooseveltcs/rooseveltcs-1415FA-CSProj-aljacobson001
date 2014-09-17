e874294decd7cac37ad5aeb541fb82e1ad520599
UltGuardian <alj@hushmail.me>
2014-09-16 20:38:26 -0700

    Testing
    testing

---
+++ src/GUITest/TestGUI.java
@@ -0,0 +1,10 @@
+package GUITest;
+
+/**
+ * Created by Aaron on 9/16/2014.
+ */
+public class TestGUI {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
