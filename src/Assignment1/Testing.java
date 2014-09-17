51f51bc696136527f2d5bbc0251056c532a15487
UltGuardian <alj@hushmail.me>
2014-09-16 20:40:29 -0700

    Testing
    Testing

---
+++ src/Assignment1/Testing.java
@@ -0,0 +1,7 @@
+package Assignment1;
+
+/**
+ * Created by Aaron on 9/16/2014.
+ */
+public class Testing {
+}
---
+++ src/GUItest/GUItest.java
@@ -0,0 +1,7 @@
+package GUItest;
+
+/**
+ * Created by Aaron on 9/16/2014.
+ */
+public class GUItest {
+}
